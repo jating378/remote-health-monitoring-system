09c0600edf3f3f51c58bbdb039b6fedc6144def1
jating378 <100723944+jating378@users.noreply.github.com>
2025-03-03 19:20:48 +0000

    Updated for deployment

--- app.py
+++ app.py
@@ -605,4 +605,4 @@ def patient_history(pid):
 # Run the Flask web server with Socket.IO
 if __name__ == '__main__':
     threading.Thread(target=simulation_thread, daemon=True).start()
-    socketio.run(app, port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
